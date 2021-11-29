67fbdefceaf9d48ca1316735cd8430dc23a85b60
yykudrin <yykudrin@gmail.com>
2021-11-29 11:29:05 +0300

    add

--- superset/userData.sh
+++ superset/userData.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 git clone https://github.com/yykudrin/aws_boto3_analytics_infrastructure.git
 cd aws_boto3_analytics_infrastructure/superset
-sudo chmod +x superset_install.sh
-sudo ./superset_install.sh
 sudo cp superset.service /lib/systemd/system/
+sudo cp superset.sh /usr/bin/
+sudo chmod +x superset_install.sh
 sudo chmod 644 /lib/systemd/system/superset.service
-cp superset.sh /usr/bin/
 sudo chmod +x /usr/bin/superset.sh
+sudo ./superset_install.sh
 sudo systemctl daemon-reload
 sudo systemctl enable superset.service
 sudo systemctl start superset.service
